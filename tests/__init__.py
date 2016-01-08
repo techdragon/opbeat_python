ab9790e1d94cbdfa2f7905f99ee4b498f459331b
jalvz <juan@opbeat.com>
2016-01-08 17:23:04 +0100

    change location of opbeat settings monkey patching

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,5 @@
+from django.conf import settings
+
 VERSION = 1.0
+
+settings.OPBEAT = {'CLIENT': 'tests.contrib.django.django_tests.TempStoreClient'}
--- tests/contrib/django/__init__.py
+++
@@ -1,3 +0,0 @@
-from django.conf import settings
-
-settings.OPBEAT = {'CLIENT': 'tests.contrib.django.django_tests.TempStoreClient'}
